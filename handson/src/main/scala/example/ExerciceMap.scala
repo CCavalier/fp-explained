d9190e8a39f7cd83b51b8faa86174fc01ff17184
Arnaud Castelltort <arnaud@codingame.com>
2017-05-03 11:50:00 +0200

    map exercise

---
+++ handson/src/main/scala/example/ExerciceMap.scala
@@ -0,0 +1,26 @@
+package example
+
+object ExerciceMap {
+  
+  /**
+   * Split all the sentences of a list l in lists of words
+   * 
+   * e.g: splitInWords(List("Hi", "Hello World", "Look who it is"))
+   * should gives:
+   * List(List(Hi), List(Hello, World), List(Look, who, it, is))
+   * 
+   * Hint: use Map
+   */
+  def splitInWords(l: List[String]) = ???
+  
+  /**
+   * 
+   * Using flatMap, count the number of Words from a list of sentences, 
+   * e.g: countWords(List("Hi", "Hello World", "Look who it is"))
+   * gives 7
+   * 
+   * 
+   */
+  def countWords(l: List[String]) = ???
+
+}
