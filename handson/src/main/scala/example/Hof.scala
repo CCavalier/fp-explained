18dbfde5c20e16b11337cfc1f0a560495fab5eb1
charlotte <charlotte@codingame.com>
2017-06-02 14:24:44 +0200

    fix compilation issue

--- handson/src/main/scala/example/Hof.scala
+++ handson/src/main/scala/example/Hof.scala
@@ -15,13 +15,13 @@ object Hof{
     * if string is not empty say something, else ask "Who are you"
    */
   def greetings = ???
- 
+
 
   /**
     * should say hello if string is empty, else ask "Who are you"
     */
-  def helloOrAskName(string: String): String = {
+  def helloOrAskName(string: String): String = ???/*{
       greetings(string, hello)
-  }
+  }*/
 
 }
