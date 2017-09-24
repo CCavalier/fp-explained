7d2db50d471bf67da6e1fac871f283616b81fb83
ccavalier <charlotte@equisense.fr>
2017-09-24 21:53:35 +0200

    fix signature

--- handson/src/main/scala/example/CurryingObject.scala
+++ handson/src/main/scala/example/CurryingObject.scala
@@ -9,7 +9,7 @@ object CurryingObject {
   def multiply(x : Int, y: Int):Int= x*y //TODO define a function who multiply x by y
 
 
-  def multiplyByTwo = ??? //TODO define a function who multiply x by y
+  def multiplyByTwo(x:Int) = ??? //TODO define a function who multiply x by y
 
 
 }
